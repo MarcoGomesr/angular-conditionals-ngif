ab0eabe91d7d5d5cd179eb842cc17e42680727a3
marcogomesr <marcogomesr@gmail.com>
2017-06-19 00:27:54 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // arr = [1,2,3,4,5,6];
+  // isTrue = true;
+  // myName: string  = "Marco";
+
+  
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    ProductComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    {
+    	path: 'member',
+    	component: MembersComponent
+    },
+    {
+    	path: 'product',
+    	component: ProductComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
